60fdf32cb0ff595a778c1359cff29be92701092b
Yuan Tong <tongyuan200097@gmail.com>
2023-06-07 23:03:53 +0800

    init

---
+++ nvinfer-dev/recipe/post-link.sh
@@ -0,0 +1,1 @@
+echo "The TensorRT Development package is non-distributable and only for private building purpose." >> "$PREFIX"/.messages.txt
---
+++ nvinfer/recipe/post-link.sh
@@ -0,0 +1,2 @@
+echo "By downloading and using the TensorRT conda packages, you accept the terms and conditions of the NVIDIA TensorRT EULA -
+  https://docs.nvidia.com/deeplearning/tensorrt/sla/index.html" >> "$PREFIX"/.messages.txt
